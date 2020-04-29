57104c186d0cd35f2dd3fa902f5ccc54a6d7626e
zhangjr <ruilovechu@live.com>
2020-04-29 19:00:27 +0800

    添加可移动对话框

--- jingruizhang-probim-vue/src/main.js
+++ jingruizhang-probim-vue/src/main.js
@@ -12,30 +12,5 @@ new Vue({
 //某些时候需要可拖动和不可拖动之间切换，使用下面的方法：
 //定义一个变量 needDrag
 //v-drag:[needDrag]="draggreet"  改变needDrag的值为true或false即可
-Vue.directive('zdrag',function (el, binding) {
-  let op = el;  //当前元素
-  let self = this; //上下文
-  op.onmousedown = function (e) {
-
-    //鼠标按下，计算当前元素距离可视区的距离
-    let disX = e.clientX - op.offsetLeft;
-    let disY = e.clientY - op.offsetTop;
-    document.onmousemove = function (e) {
-      if (binding.arg != false) {
-        //通过事件委托，计算移动的距离
-        let l = e.clientX - disX;
-        let t = e.clientY - disY;
-        //移动当前元素
-        op.style.left = l + 'px';
-        op.style.top = t + 'px';
-        //将此时的位置传出去
-        binding.value({x:e.pageX,y:e.pageY})
-      }
-    };
-    document.onmouseup = function (e) {
-      document.onmousemove = null;
-      document.onmouseup = null;
-    };
-  };
-}
-);
+// Vue.directive('zdrag',
+// );
--- jingruizhang-probim-vue/src/myPlugin/zdialog/zdialog-function.vue
+++ jingruizhang-probim-vue/src/myPlugin/zdialog/zdialog-function.vue
@@ -90,6 +90,34 @@ export default {
         }
 
     },
+    directives:{
+        'zdrag':function (el, binding) {
+            let op = el;  //当前元素
+            let self = this; //上下文
+            op.onmousedown = function (e) {
+
+                //鼠标按下，计算当前元素距离可视区的距离
+                let disX = e.clientX - op.offsetLeft;
+                let disY = e.clientY - op.offsetTop;
+                document.onmousemove = function (e) {
+                if (binding.arg != false) {
+                    //通过事件委托，计算移动的距离
+                    let l = e.clientX - disX;
+                    let t = e.clientY - disY;
+                    //移动当前元素
+                    op.style.left = l + 'px';
+                    op.style.top = t + 'px';
+                    //将此时的位置传出去
+                    binding.value({x:e.pageX,y:e.pageY})
+                }
+                };
+                document.onmouseup = function (e) {
+                document.onmousemove = null;
+                document.onmouseup = null;
+                };
+            };
+        }
+    },
     mounted(){
 
         // 使用外部传递的变量初始化私有变量
